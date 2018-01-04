1c1daad2dac77652d5fd57785bf7500bae26ac2f
Noah Kim <nk8na@virginia.edu>
2018-01-03 21:04:03 -0500

    Implement parsing

---
+++ retainer.c
@@ -0,0 +1,67 @@
+#include "mpc.h"
+
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+	  fputs(prompt, stdout);
+	    fgets(buffer, 2048, stdin);
+	      char* cpy = malloc(strlen(buffer)+1);
+	        strcpy(cpy, buffer);
+		  cpy[strlen(cpy)-1] = '\0';
+		    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+int main(int argc, char** argv) {
+	/* Create Some Parsers */
+	mpc_parser_t* Number   = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr     = mpc_new("expr");
+	mpc_parser_t* Lispy    = mpc_new("lispy");
+
+	mpca_lang(MPCA_LANG_DEFAULT,
+			"								\
+				number   : /-?[0-9]+/;					\
+				operator : '+' | '-' | '/' | '*';			\
+				expr     : <number> | '(' <operator> <expr>+ ')';	\
+			        lispy	 : /^/ <operator> <expr>+ /$/;			\
+			",
+	Number, Operator, Expr, Lispy);
+		
+		
+	puts("Lispy Version 0.0.0.0.1");
+	puts("Press Ctrl+c to Exit\n");
+	       
+	while (1) {
+		/* Now in either case readline will be correctly defined */
+		char* input = readline("retainer> ");
+		add_history(input);
+
+		mpc_result_t r;
+		if(mpc_parse("<stdin>", input, Lispy, &r)){
+			/* On success Print the AST */
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		} else {
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
+  		
+		free(input);
+ 	}
+	mpc_cleanup(4, Number, Operator, Expr, Lispy);
+        return 0;
+}
